86ad3f092cc86916d57c8f820ea0adcaec017d8f
Peter <plithner@gmail.com>
2018-06-14 12:04:54 +0200

    Added error page when database connection fails

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -31,6 +31,7 @@ else:
 redis_server = os.environ['REDIS']
 
 # Redis Connection
+db_conn = True
 try:
     if "REDIS_PWD" in os.environ:
         r = redis.StrictRedis(host=redis_server,
@@ -39,61 +40,65 @@ try:
     else:
         r = redis.Redis(redis_server)
     r.ping()
-except redis.ConnectionError:
-    exit('Failed to connect to Redis')
+#except redis.ConnectionError:
+except (redis.exceptions.ConnectionError, socket.gaierror) as e:
+    print("Failed to connect to Redis")
+    db_conn = False
 
      
-
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
     title = socket.gethostname()
 
 # Init Redis
-if not r.get(button1): r.set(button1,0)
-if not r.get(button2): r.set(button2,0)
+if db_conn:
+    if not r.get(button1): r.set(button1,0)
+    if not r.get(button2): r.set(button2,0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    try:
-        if request.method == 'GET':
+    if db_conn:
+        try:
+            if request.method == 'GET':
 
-            # Get current values
-            vote1 = r.get(button1).decode('utf-8')
-            print ("Current values-Vote1: ", vote1)
-            vote2 = r.get(button2).decode('utf-8')            
-
-            # Return index with values
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-
-        elif request.method == 'POST':
-
-            if request.form['vote'] == 'reset':
-                
-                # Empty table and return results
-                r.set(button1,0)
-                r.set(button2,0)
-                vote1 = r.get(button1).decode('utf-8')
-                print ("reset-Vote1: ", vote1)
-                vote2 = r.get(button2).decode('utf-8')
-                return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-            
-            else:
-
-                # Insert vote result into DB
-                vote = request.form['vote']
-                r.incr(vote,1)
-                
                 # Get current values
                 vote1 = r.get(button1).decode('utf-8')
-                print ("voteVote1: ", vote1)
-                vote2 = r.get(button2).decode('utf-8')  
-                    
-                # Return results
+                print ("Current values-Vote1: ", vote1)
+                vote2 = r.get(button2).decode('utf-8')            
+
+                # Return index with values
                 return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
-    except (redis.exceptions.ConnectionError, socket.gaierror) as e:
-        print("Failed to connect to redis")
-    
+            elif request.method == 'POST':
+
+                if request.form['vote'] == 'reset':
+                    
+                    # Empty table and return results
+                    r.set(button1,0)
+                    r.set(button2,0)
+                    vote1 = r.get(button1).decode('utf-8')
+                    print ("reset-Vote1: ", vote1)
+                    vote2 = r.get(button2).decode('utf-8')
+                    return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+                
+                else:
+
+                    # Insert vote result into DB
+                    vote = request.form['vote']
+                    r.incr(vote,1)
+                    
+                    # Get current values
+                    vote1 = r.get(button1).decode('utf-8')
+                    print ("voteVote1: ", vote1)
+                    vote2 = r.get(button2).decode('utf-8')  
+                        
+                    # Return results
+                    return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+
+        except (redis.exceptions.ConnectionError, socket.gaierror) as e:
+            return render_template("db_error.html")
+    else:
+        return render_template("db_error.html")
 
 if __name__ == "__main__":
     app.run()
